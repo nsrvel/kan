c86f5832e4c09de5879606933ae10984e3506bca
putra <putra.rama@suretybond.co.id>
2023-12-14 10:10:53 +0700

    initial commit

--- src/components/contact-button.tsx
+++ src/components/contact-button.tsx
@@ -6,7 +6,7 @@ const ContactButton = () => {
         <button className="group bg-primary rounded-3xl border-2 border-white">
             <div className="flex flex-row items-center justify-center gap-x-2 px-5 py-3">
                 <p className="text-md text-white font-semibold">Hubungi Kami</p>
-                <Image alt="contact-logo" src="icons/paper-plane.svg" height={22} width={20} color="primary" />
+                <Image alt="contact-logo" src="/icons/paper-plane.svg" height={22} width={20} color="primary" />
             </div>
         </button>
     );
